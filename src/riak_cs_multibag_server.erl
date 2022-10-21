650b0bc2c0171453a769dc0b2247586d48942dea
Andrei Zavada <andrei.zavada@tiot.jp>
2022-10-21 22:54:31 +0300

    drop lager

--- src/riak_cs_multibag_server.erl
+++ src/riak_cs_multibag_server.erl
@@ -70,7 +70,7 @@ handle_call(Request, _From, State) ->
 handle_cast({new_weights, Weights}, State) ->
     NewState = update_weight_state(Weights, State),
     %% TODO: write log only when weights are updated.
-    %% lager:info("new_weights: ~p~n", [NewState]),
+    %% logger:info("new_weights: ~p~n", [NewState]),
     {noreply, NewState};
 handle_cast(_Msg, State) ->
     {noreply, State}.
